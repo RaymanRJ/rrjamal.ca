6917af9772faa5c1c4d998b2b9431855935ae89d
raymanrj <raymanrj@live.com>
2021-06-09 20:26:44 -0400

    Starting DB Connections

---
+++ restapi/DB/DBConnection.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+import psycopg2
+from DBConfig import db_config
+
+
+class DBConnection:
+    __config: Dict[str, str]
+    __conn: psycopg2
+
+    def __init__(self):
+        self.__config = db_config
+        self.__conn = psycopg2.connect(
+            host=self.__config['host'],
+            database=self.__config['database'],
+            user=self.__config['user'],
+            password=self.__config['pass']
+        )
+
+    @property
+    def connection(self) -> psycopg2:
+        return self.__conn
