35c73d0b26738b757535f86a14e61a90d5f0f762
raymanrj <raymanrj@live.com>
2021-06-11 00:02:07 -0400

    Staring homepage carousel

--- react-app/rrjamal-app/src/App.js
+++ react-app/rrjamal-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+<<<<<<< HEAD
 import Navigation from './components/navigation/navigation-bar'
 import {
   BrowserRouter
@@ -9,6 +10,31 @@ function App() {
     <BrowserRouter>
       <Navigation />
     </BrowserRouter>
+=======
+import Navigation from './components/nav/nav';
+import ControlledCarousel from './components/carousel/carousel'
+
+function App() {
+  return (
+    <div className="App">
+     <Navigation /> 
+     <ControlledCarousel />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> Staring homepage carousel
   );
 }
 
