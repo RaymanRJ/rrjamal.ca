ccd622b67c4ca6ebd3143c80614652cd47116ade
raymanrj <raymanrj@live.com>
2021-06-11 00:02:07 -0400

    Staring homepage carousel

--- react-app/rrjamal-app/src/App.js
+++ react-app/rrjamal-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/nav/nav';
+import ControlledCarousel from './components/carousel/carousel'
 
 function App() {
   return (
     <div className="App">
      <Navigation /> 
+     <ControlledCarousel />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
