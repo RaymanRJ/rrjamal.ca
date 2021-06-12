78354b58484bbc981074b4e1171ba807ec334543
raymanrj <raymanrj@live.com>
2021-06-11 23:02:08 -0400

    Dockerizing restapi and pusing to ECR

--- restapi/src/Main.py
+++ restapi/src/Main.py
@@ -9,4 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_debugger=False, use_reloader=True, passthrough_errors=True, host='0.0.0.0')
+    app.run(debug=True, use_debugger=False, use_reloader=True, passthrough_errors=True)
--- restapi/update_config.py
+++ restapi/update_config.py
@@ -6,8 +6,11 @@ HOST = os.getenv('DB_HOST')
 DB = os.getenv('DB')
 USER = os.getenv('DB_USER')
 PASS = os.getenv('DB_PASS')
+TEST = os.getenv('TEST_SECRET')
 
-config_file='dbsettings.config.yml'
+print(f"Captured Test_Secret value from GitHub Actions: {TEST}")
+
+config_file = './src/dbsettings.config.yml'
 with open(config_file, 'r') as ymlfile:
     loaded = yaml.load(ymlfile, Loader=Loader)
 
