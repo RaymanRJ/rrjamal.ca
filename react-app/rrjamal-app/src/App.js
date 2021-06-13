d04355cca03c026b10c7c748d9803503c7f88802
raymanrj <raymanrj@live.com>
2021-06-12 23:03:11 -0400

    Started router

--- react-app/rrjamal-app/src/App.js
+++ react-app/rrjamal-app/src/App.js
@@ -1,27 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/nav/nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <Navigation /> 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Profile() {
+  return <h2>Profile</h2>;
+}
+
+function Projects() {
+  return <h2>Projects</h2>;
 }
 
 export default App;
