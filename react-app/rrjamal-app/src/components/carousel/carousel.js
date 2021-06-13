d2a5c07f8f6ff304d36d750594dc109746704f09
raymanrj <raymanrj@live.com>
2021-06-12 22:54:46 -0400

    wip -> centering image

--- react-app/rrjamal-app/src/components/carousel/carousel.js
+++ react-app/rrjamal-app/src/components/carousel/carousel.js
@@ -1,25 +1,7 @@
 import { React, useState } from 'react'
 import { Container, Carousel, Image, Row, Col } from 'react-bootstrap'
-import dongdaemun from '../../assets/images/dongdaemun-design-plaza-leds.jpg'
-import olympic_park from '../../assets/images/seoul-olympic-park.jpg'
-import shibuya from '../../assets/images/shibuya-crossing.jpg'
-import shinjuku from '../../assets/images/shinjuku-goyen.jpg'
-import tian_tan from '../../assets/images/tian-tan-buddha.jpg'
-import tiger_temple from '../../assets/images/tiger-temple.jpg'
-import tiger_temple_2 from '../../assets/images/tiger-temple-2.jpg'
-import waujeongsa from '../../assets/images/waujeongsa-temple.jpg'
+import images from './images'
 import './carousel.css';
-
-const images = [
-    {image: dongdaemun, title: "Dongdaemun Design Plaza"},
-    {image: olympic_park, title: "Seoul Olympic Park"},
-    {image: shibuya, title: "Shibuya Crossing"},
-    {image: shinjuku, title: "Shinjuku Goyen Park"},
-    {image: tian_tan, title: "Tian Tan Buddha"},
-    {image: tiger_temple, title: "Tiger Temple"},
-    {image: tiger_temple_2, title: "Tiger Temple"},
-    {image: waujeongsa, title: "Waujeonsa Temple"}
-]
 
 function ControlledCarousel() {
     const [index, setIndex] = useState(0);
@@ -32,24 +14,29 @@ function ControlledCarousel() {
         <Container fluid>
             <Row>
                 <Col></Col>
-                <Col xs={6}>
-                    <Carousel activeIndex={index} onSelect={handleSelect}>
-                        {
-                            images.map((image, i) => {
-                                return (
-                                    <Carousel.Item 
-                                    key={i}>
-                                        <Image
-                                        src={image.image}
-                                        fluid>
-                                        </Image>
-                                        <Carousel.Caption>
-                                            <h3>{image.title}</h3>
-                                        </Carousel.Caption>
-                                    </Carousel.Item>)
-                            })
-                        }
-                    </Carousel></Col>
+                <Col xs={8}>
+                    <div>
+                        <Carousel activeIndex={index} onSelect={handleSelect}>
+                            {
+                                images.map((image, i) => {
+                                    return (
+                                        <Carousel.Item 
+                                        key={i}>
+                                            <Image
+                                            src={image.image}
+                                            fluid>
+                                            </Image>
+                                            <Carousel.Caption>
+                                                <a href={image.url} target={"_blank"}>
+                                                    <h3>{image.title}</h3>
+                                                </a>
+                                            </Carousel.Caption>
+                                        </Carousel.Item>)
+                                })
+                            }
+                        </Carousel>
+                    </div>
+                </Col>
                 <Col></Col>
             </Row>
         </Container>
---
+++ react-app/rrjamal-app/src/components/carousel/images.js
@@ -0,0 +1,46 @@
+
+import dongdaemun from '../../assets/images/dongdaemun-design-plaza-leds.jpg'
+import olympic_park from '../../assets/images/seoul-olympic-park.jpg'
+import shibuya from '../../assets/images/shibuya-crossing.jpg'
+import shinjuku from '../../assets/images/shinjuku-goyen.jpg'
+import big_buddha from '../../assets/images/phuket-big-buddha.jpg'
+import tiger_temple from '../../assets/images/tiger-temple.jpg'
+import tiger_temple_2 from '../../assets/images/tiger-temple-2.jpg'
+import waujeongsa from '../../assets/images/waujeongsa-temple.jpg'
+
+const images = [
+    {
+        image: dongdaemun, 
+        title: "Dongdaemun Design Plaza",
+        url: "https://english.visitseoul.net/shopping/Dongdaemun-Design-Plaza-DDP_/96"
+    },
+    {
+        image: olympic_park, 
+        title: "Seoul Olympic Park",
+        url: "https://english.visitseoul.net/nature/Olympic-Park_/2140"
+    },
+    {
+        image: shibuya, 
+        title: "Shibuya Crossing",
+        url: "https://en.japantravel.com/tokyo/shibuya-crossing/3016"
+    },
+    {
+        image: shinjuku, 
+        title: "Shinjuku Goyen Park",
+        url: "https://en.japantravel.com/tokyo/shinjuku-gyoen-4k/22201"
+    },
+    {
+        image: big_buddha, 
+        title: "Phuket Big Buddha",
+        url: "https://www.phuket101.net/phuket-big-buddha/"
+    },
+    {
+        image: waujeongsa, 
+        title: "Waujeonsa Temple",
+        url: "https://nothingaboutsomething.com/korea/waujeongsa-temple"
+    },
+    // {image: tiger_temple, title: "Tiger Temple"},
+    // {image: tiger_temple_2, title: "Tiger Temple"}
+]
+
+export default images;
