9e25b2488a7f050aafdce588507179efb0a0c0a8
raymanrj <raymanrj@live.com>
2021-06-22 23:29:09 -0400

    Working on Projects page

--- react-app/rrjamal-app/src/components/projects/projects.js
+++ react-app/rrjamal-app/src/components/projects/projects.js
@@ -1,7 +1,50 @@
 import React from 'react'
 
-function Projects() {
-    return ("Projects")
-};
+class Projects extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
 
-export default Projects
+    componentDidMount() {
+        fetch("https://api.github.com/users/RaymanRJ/repos")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded:true,
+                        items: result.map(result => result.id)
+                    });
+                    console.log(this.state);
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if(error){
+            return <div>Error: {error.message}</div>;
+        }
+        else if (!isLoaded){
+            console.log("Not loaded");
+            return <div>Loading...</div>;
+        } else{
+            console.log(items);
+            return <ul>
+                {items}
+            </ul>
+        }
+    };
+}
+
+export default Projects;
