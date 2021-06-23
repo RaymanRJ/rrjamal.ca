8d40682dee123cf55f00d0b2fb7a662f082fad40
raymanrj <raymanrj@live.com>
2021-06-22 22:24:07 -0400

    Minor adjustments to styling

--- react-app/rrjamal-app/src/components/carousel/carousel.js
+++ react-app/rrjamal-app/src/components/carousel/carousel.js
@@ -3,39 +3,46 @@ import { Container, Carousel, Image, Row, Col } from 'react-bootstrap'
 import images from './images'
 import './carousel.css';
 
-function ControlledCarousel() {
+function GetCarousel(){
     const [index, setIndex] = useState(0);
   
     const handleSelect = (selectedIndex, e) => {
       setIndex(selectedIndex);
     };
-  
+    
+    return (
+        <div>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                {
+                    images.map((image, i) => {
+                        return (
+                            <Carousel.Item 
+                            key={i}>
+                                <Image
+                                src={image.image}
+                                fluid
+                                rounded>
+                                </Image>
+                                <Carousel.Caption>
+                                    <a href={image.url} target={"_blank"}>
+                                        <h4>{image.title}</h4>
+                                    </a>
+                                </Carousel.Caption>
+                            </Carousel.Item>)
+                    })
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+function ControlledCarousel() {
     return (
         <Container fluid>
             <Row>
                 <Col></Col>
                 <Col xs={8}>
-                    <div>
-                        <Carousel activeIndex={index} onSelect={handleSelect}>
-                            {
-                                images.map((image, i) => {
-                                    return (
-                                        <Carousel.Item 
-                                        key={i}>
-                                            <Image
-                                            src={image.image}
-                                            fluid>
-                                            </Image>
-                                            <Carousel.Caption>
-                                                <a href={image.url} target={"_blank"}>
-                                                    <h3>{image.title}</h3>
-                                                </a>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>)
-                                })
-                            }
-                        </Carousel>
-                    </div>
+                    { GetCarousel() }
                 </Col>
                 <Col></Col>
             </Row>
