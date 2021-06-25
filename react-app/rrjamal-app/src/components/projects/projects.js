3e3f57a01e9310c41f678355346851e8776087a5
raymanrj <raymanrj@live.com>
2021-06-24 23:35:15 -0400

    Moving GH repos to cards

--- react-app/rrjamal-app/src/components/projects/projects.js
+++ react-app/rrjamal-app/src/components/projects/projects.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+import './projects.css'
 
 class Projects extends React.Component {
     constructor(props) {
@@ -8,7 +11,39 @@ class Projects extends React.Component {
         isLoaded: false,
         items: []
       };
+    };
+
+    getProjectCard(project){
+        return(
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>{project.name}</Card.Title>
+                    <Card.Text>
+                        {project.description}
+                    </Card.Text>
+                    <Card.Link href={project.html_url} target="_blank">Visit GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+        )
     }
+
+    getProjectDiv(items){
+        return (
+            <div className="page-background">
+                <Container>
+                    <Row>
+                        <Col></Col>
+                        <Col xs={8}>
+                            {
+                                items.map(item => <div>{this.getProjectCard(item)}</div>)
+                            }
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    };
 
     componentDidMount() {
         fetch("https://api.github.com/users/RaymanRJ/repos")
@@ -17,16 +52,18 @@ class Projects extends React.Component {
                 (result) => {
                     this.setState({
                         isLoaded:true,
-                        items: result.map(item => ({ 
+                        items: result
+                        .map(item => ({ 
                             id: item.id,
                             name: item.name,
                             full_name: item.full_name,
                             html_url: item.html_url,
                             description: item.description,
-                            url: item.url,
-                            created_at: item.created_at,
-                            updated_at: item.updated_at
+                            created_at: new Date(item.created_at),
+                            updated_at: new Date(item.updated_at),
+                            pushed_at: new Date(item.pushed_at)
                         }))
+                        .sort((a, b) => b.pushed_at - a.pushed_at)
                     });
                 },
                 (error) => {
@@ -40,17 +77,12 @@ class Projects extends React.Component {
 
     render() {
         const { error, isLoaded, items } = this.state;
-        if(error){
+        if(error)
             return <div>Error: {error.message}</div>;
-        }
-        else if (!isLoaded){
-            console.log("Not loaded");
+        else if (!isLoaded)
             return <div>Loading...</div>;
-        } else{
-            return <ul>
-                {items.map(item => <li key={item.id}>{item.name}</li>)}
-            </ul>
-        }
+        else
+            return this.getProjectDiv(items);
     };
 }
 
