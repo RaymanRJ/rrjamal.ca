af3b284c5c4c1e0513191e3b7332dde9fe28b67f
Rayman Jamal <RaymanRJ@Live.com>
2023-09-22 21:42:00 -0400

    Update wording

--- front-end/src/components/LeftColumn/Bio/Bio.js
+++ front-end/src/components/LeftColumn/Bio/Bio.js
@@ -7,7 +7,8 @@ function Bio() {
         <div className="bio">
             <h1>Rayman Jamal</h1>
             <h3>Software Engineer based in Toronto</h3>
-            <p>I type things into my machine. Things then happen in other machines. Sometimes those things even happen as planned.</p>
+            <p>I type things into my machine. Things then happen in other machines. 
+                Sometimes, those things even happen as planned.</p>
         </div>
     );
 }
