118a94b7591f792b42f5507b728b2dc32ba6bb14
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2023-09-28 00:07:50 -0400

    Update CICD Policies (#14)

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -23,3 +23,33 @@ data "aws_iam_policy_document" "allow_access_from_cloudfront" {
     ]
   }
 }
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "${var.stack_name}-github-actions-policy"
+  description = "GitHub Actions Policy for ${var.stack_name}"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid      = "VisualEditor0"
+        Effect   = "Allow"
+        Action   = ["s3:PutObject", "s3:ListBucket", "s3:GetObject", "cloudfront:CreateInvalidation"]
+        Resource = [
+          "${var.cloudfront_arn}",
+          "${var.s3_arn}",
+          "${var.s3_arn}/*",
+          "${var.s3_asset_arn}",
+          "${var.s3_asset_arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach the IAM policy to an existing group
+resource "aws_iam_policy_attachment" "ci_cd_policy_attachment" {
+  name       = "Attach ${var.stack_name} GitHub Actions Policy to existing group"
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+  groups     = [var.cicd_actions_group_name]
+}
--- terraform/iam/variables.tf
+++ terraform/iam/variables.tf
@@ -1,9 +1,24 @@
+variable "stack_name" {
+    description = "Stack Name"
+    type = string
+}
+
 variable "s3_arn" {
     description = "The ARN of the S3 bucket"
     type = string
 }
 
+variable "s3_asset_arn" {
+    description = "The ARN of the S3 bucket for assets"
+    type = string
+}
+
 variable "cloudfront_arn" {
     description = "The ARN of the CloudFront distribution"
+    type = string
+}
+
+variable "cicd_actions_group_name" {
+    description = "The name of the IAM group for CICD actions"
     type = string
 }
